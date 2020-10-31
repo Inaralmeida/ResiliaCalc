ed88fe6e01fcc3388338cc17dc302c0389df2450
inralmeida <inara.ralmeida@gmail.com>
2020-10-31 16:35:01 -0300

    Adicionei os numero ao input

--- js/script_calculadora.js
+++ js/script_calculadora.js
@@ -1,3 +1,5 @@
+ var input = document.getElementById('input')
+ var historico = document.getElementById('historico')
  var digito_1 = document.getElementById("digito_1")
  var digito_2 = document.getElementById("digito_2")
  var digito_3 = document.getElementById("digito_3")
@@ -23,4 +25,95 @@
  var limpa_telaCE = document.getElementById("limpa_telaCE")
  var botao_resultado = document.getElementById("botao_resultado")
  
+ 
+ let salvaNumero = []
+  let salvaNum= salvaNumero.join('')
+ 
+digito_1.addEventListener('click', function(){
+  input.innerText += 1
+  salvaNumero.push(1)  
+})
+digito_2.addEventListener('click', function(){
+  input.innerText +=  2
+  salvaNumero.push(2)
+})
+digito_3.addEventListener('click', function(){
+  input.innerText += 3
 
+})
+digito_4.addEventListener('click', function(){
+  input.innerText += 4
+})
+
+digito_5.addEventListener('click', function(){
+  input.innerText += 5
+  
+})
+digito_6.addEventListener('click', function(){
+  input.innerText += 6
+})
+digito_7.addEventListener('click', function(){
+  input.innerText += 7
+})
+digito_8.addEventListener('click', function(){
+  input.innerText += 8
+  
+})
+digito_9.addEventListener('click', function(){
+  input.innerText += 9
+})
+digito_0.addEventListener('click', function(){
+  input.innerText += 0
+})
+operador_soma.addEventListener('click', function(){
+  salvaNumero.push(' ')
+  input.innerText += ' +  '
+  historico.innerText = salvaNum
+  
+  input.textContent = ' '
+})
+
+console.log(salvaNum)
+
+/* function falsoBinario(x){
+  let numero = x.toString()
+  console.log(numero.length)
+  
+  let resultado = []
+  let resultadoFinal = resultado.join('')
+  for(let i = 0; i < numero.length; i++){
+    
+    if(x[i] > 4){
+      let num5 = 5
+      resultado.push(num5)
+    }
+    if(x[i] < 5) {
+      let num0 = 0
+      resultado.push(num0)
+      console.log(resultado)
+    }
+  }
+  
+  console.log( resultadoFinal)
+}
+
+falsoBinario("351772248482")
+
+function inverter(n) {
+  let numeroLetra = n.toString()
+  let arrOriginal = []
+  let arrInvertido = []
+  
+  for(let i = 0; i < numeroLetra.length; i++){
+    arrOriginal.push(parseInt(numeroLetra[i]))
+    console.log(numeroLetra[i])
+  }
+  
+  for(let i = arrOriginal.length -1  ; i > -1; i--){
+    arrInvertido.push(parseInt(numeroLetra[i]))
+    console.log(numeroLetra[i])
+  }
+  console.log(arrInvertido)
+}
+
+inverter(351772248482) */
